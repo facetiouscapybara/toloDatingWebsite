ec093b55a94e6a827b826e99ad0e5bf92907ff56
Eric Geniesse <egeniesse@gmail.com>
2016-02-10 12:31:01 -0800

    took out morgan

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,9 +1,7 @@
 var bodyParser = require('body-parser');
-var morgan = require('morgan');
 
 module.exports = function(app, express) {
   app.use(express.static(__dirname + '../../../staticPages/'));
-  app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
   app.use(function (err, req, res, next) {
