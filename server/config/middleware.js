29c7b3c17e5b3ad5f85822e642abe1698a4a8c6d
Eric Geniesse <egeniesse@gmail.com>
2016-02-10 12:26:51 -0800

    moved the static files up the file

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -2,10 +2,10 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 
 module.exports = function(app, express) {
+  app.use(express.static(__dirname + '../../../staticPages/'));
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
-  app.use(express.static(__dirname + '../../../staticPages/'));
   app.use(function (err, req, res, next) {
     if (err.name === 'UnauthorizedError') {
       res.status(401).send('invalid token');
