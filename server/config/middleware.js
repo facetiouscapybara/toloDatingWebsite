b05b23682d9ac4b1f51e2a4828afbc28652fa474
Eric Geniesse <egeniesse@gmail.com>
2016-02-12 12:37:06 -0800

    Taking out body parser as a dependency

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -2,11 +2,4 @@ var bodyParser = require('body-parser');
 
 module.exports = function(app, express) {
   app.use(express.static(__dirname + '../../../staticPages/'));
-  app.use(bodyParser.urlencoded({extended: true}));
-  app.use(bodyParser.json());
-  app.use(function (err, req, res, next) {
-    if (err.name === 'UnauthorizedError') {
-      res.status(401).send('invalid token');
-    }
-  });
 };
