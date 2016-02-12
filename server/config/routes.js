757a368b790f2c87098a194d81497e7d23f9772e
Eric Geniesse <egeniesse@gmail.com>
2016-02-12 14:33:54 -0800

    Adding Dan's bio to the page

--- server/config/routes.js
+++ server/config/routes.js
@@ -5,6 +5,9 @@ module.exports = function(app, express) {
   app.get('/reference', function(req, res){
     res.sendFile(path.join(__dirname + './../../staticPages/html/reference.html'));
   });
+  app.get('/favicon.ico', function(req, res){
+  	res.sendFile(path.join(__dirname + './../../staticPages/assets/images/ToloFavicon.png'));
+  });
   app.get('*', function(req,res){
     console.log(__dirname)
     res.sendFile(path.join(__dirname + './../../staticPages/html/splash.html'));
